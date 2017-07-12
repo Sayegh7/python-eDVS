b6fbc240531918700d911ca62a1daa287a8a8b8c
sayegh <ahmedelsayegh7@gmail.com>
2017-07-12 16:18:36 +0200

    Viewer working

--- temp.py
+++ temp.py
@@ -2,7 +2,8 @@ import numpy as np
 import cv2
 import serial
 import struct
-from PyQt4.QtCore import QThread, SIGNAL
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 import sys
 from PyQt4 import QtGui
 import matplotlib.pyplot as plt
@@ -19,7 +20,7 @@ class computationThread(QThread):
         self.image = QtGui.QImage(128, 128, QtGui.QImage.Format_RGB32)
 
         while ser.is_open:
-            line = ser.read(512)
+            line = ser.read(4096)
             self.image.fill(QtGui.qRgb(255,255,255))
             for i,k in zip(line[0::2], line[1::2]):
                 byteOne = struct.unpack('B', i)[0]
@@ -33,10 +34,44 @@ class computationThread(QThread):
                 x = byteTwo & 0x7f;
                 polarity = (byteTwo & 0x80) >> 7;
                 if polarity == 1:    
-                    self.image.setPixel(x,y, QtGui.qRgb(255,0,0))   
+                    self.image.setPixel(x,y, QtGui.qRgb(0,0,0))   
                 else:
-                    self.image.setPixel(x,y, QtGui.qRgb(0,255,0))  
+                    self.image.setPixel(x,y, QtGui.qRgb(0,0,0))  
+            array = self.QImageToCvMat(self.image)
+            edges = cv2.Canny(array,255,255/3,apertureSize = 3)
+            minLineLength = 1
+            maxLineGap = 50
+            lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+            print lines
+            if np.any(lines) == None:
+                self.emit(SIGNAL('reset'), self.image)
+                continue
+            for x1,y1,x2,y2 in lines[0]:
+                cv2.line(self.image,(x1,y1),(x2,y2),(0,255,0),2)
+                painter = QPainter()
+                painter.setPen(QColor(qRgb(0,255,0)))
+                painter.begin(self.image)
+                painter.drawLine(QLine(QPoint(x1,y1), QPoint(x2,y2), QtGui.qRgb(0,255,0)))
+                painter.end()
+
+            print "Found a line"
             self.emit(SIGNAL('reset'), self.image)
+
+
+    def QImageToCvMat(self, incomingImage):
+        '''  Converts a QImage into an opencv MAT format  '''
+
+        incomingImage = incomingImage.convertToFormat(4)
+        
+        width = incomingImage.width()
+        height = incomingImage.height()
+        
+        ptr = incomingImage.bits()
+        ptr.setsize(incomingImage.byteCount())
+        arr = np.array(ptr).reshape(height, width, 4)  #  Copies the data
+        return arr
+
+
 
 class Window(QtGui.QMainWindow):
     def __init__(self):
@@ -60,11 +95,6 @@ class Window(QtGui.QMainWindow):
         self.get_thread = computationThread()
         self.get_thread.start()
         self.connect(self.get_thread, SIGNAL("reset"), self.reset)
-    def setPixel(self, x, y, polarity):
-        if polarity == 1:    
-            self.image.setPixel(x,y, QtGui.qRgb(255,0,0))   
-        else:
-            self.image.setPixel(x,y, QtGui.qRgb(0,255,0))               
     def reset(self, image):
         tempPixmap = QtGui.QPixmap(1,1)
         tempPixmap.convertFromImage(image)
@@ -77,53 +107,24 @@ def main():
     form.show()
     app.exec_()
 
-#if __name__ == '__main__':
-    #main()
+if __name__ == '__main__':
+    main()
 
 
 
 ### WIP
 
-def QImageToCvMat(incomingImage):
-    '''  Converts a QImage into an opencv MAT format  '''
-
-    incomingImage = incomingImage.convertToFormat(QtGui.QImage.Format_RGB32)
-
-    width = incomingImage.width()
-    height = incomingImage.height()
-
-    Matrix = [[0 for x in range(width)] for y in range(height)] 
-    for index in Matrix:
-        for i, val in value:
-            Matrix[index][i] =  incomingImage.pixel(index, i).red()
-    
-    return Matrix
 
 
-imgpath = 'C:/Users/sayegh/test.jpg'
-img = cv2.imread(imgpath)
-image = QtGui.QImage(128, 128, QtGui.QImage.Format_RGB32)
-image.setPixel(50,0, QtGui.qRgb(0,0,0))   
-image.setPixel(50,1, QtGui.qRgb(0,0,0))   
-image.setPixel(50,2, QtGui.qRgb(0,0,0))   
-image.setPixel(50,3, QtGui.qRgb(0,0,0))   
-image.setPixel(50,4, QtGui.qRgb(0,0,0))   
-image.setPixel(50,5, QtGui.qRgb(0,0,0))   
-image.setPixel(50,6, QtGui.qRgb(0,0,0))   
-image.setPixel(50,7, QtGui.qRgb(0,0,0))   
-image.setPixel(50,8, QtGui.qRgb(0,0,0))   
-image.setPixel(50,9, QtGui.qRgb(0,0,0))   
-image.setPixel(50,10, QtGui.qRgb(0,0,0))   
-image.setPixel(50,11, QtGui.qRgb(0,0,0))
-
-doneImg = QImageToCvMat(image)
-print doneImg
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,50,150,apertureSize = 3)
-minLineLength = 10
-maxLineGap = 10
-lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
-for x1,y1,x2,y2 in lines[0]:
-    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-
-cv2.imwrite('hough.jpg',img)
+#imgpath = 'C:/Users/sayegh/test.jpg'
+#img = cv2.imread(imgpath)
+#gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#edges = cv2.Canny(gray,50,150,apertureSize = 3)
+#minLineLength = 10
+#maxLineGap = 10
+#lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+#print lines
+#for x1,y1,x2,y2 in lines[0]:
+#    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+#
+#cv2.imwrite('hough.jpg',img)
