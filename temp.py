8c09a66bed2715133ff2d292bd02c55aa0fdd3bc
sayegh <ahmedelsayegh7@gmail.com>
2017-07-17 16:55:40 +0200

    Using contours to detect lines

--- temp.py
+++ temp.py
@@ -18,6 +18,7 @@ class computationThread(QThread):
         ser = serial.Serial('COM3', 12000000, timeout=1)
         ser.write('E+\nE+')
         self.image = QtGui.QImage(128, 128, QtGui.QImage.Format_RGB32)
+        self.painter = QPainter()
 
         while ser.is_open:
             line = ser.read(4096)
@@ -38,23 +39,24 @@ class computationThread(QThread):
                 else:
                     self.image.setPixel(x,y, QtGui.qRgb(0,0,0))  
             array = self.QImageToCvMat(self.image)
-            edges = cv2.Canny(array,255,255/3,apertureSize = 3)
-            minLineLength = 1
-            maxLineGap = 50
-            lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
-            print lines
+            lines = self.detectLines(array)
             if np.any(lines) == None:
                 self.emit(SIGNAL('reset'), self.image)
                 continue
-            for x1,y1,x2,y2 in lines[0]:
-                cv2.line(self.image,(x1,y1),(x2,y2),(0,255,0),2)
-                painter = QPainter()
-                painter.setPen(QColor(qRgb(0,255,0)))
-                painter.begin(self.image)
-                painter.drawLine(QLine(QPoint(x1,y1), QPoint(x2,y2), QtGui.qRgb(0,255,0)))
-                painter.end()
+            self.painter.begin(self.image)
+            self.painter.setPen(QColor(qRgb(0,255,0)))
 
-            print "Found a line"
+            for line in lines:
+                for x1,y1,x2,y2 in line:
+                    if y1 > 96 and y2 > 96:
+                        if x1 <= 64 or x2 <= 64:
+                            self.painter.setPen(QColor(qRgb(255,255,0)))
+                        else:
+                            self.painter.setPen(QColor(qRgb(255,0,0)))                            
+                    else:
+                        self.painter.setPen(QColor(qRgb(0,255,0)))
+                    self.painter.drawLine(x1,y1,x2,y2)
+            self.painter.end()
             self.emit(SIGNAL('reset'), self.image)
 
 
@@ -70,8 +72,27 @@ class computationThread(QThread):
         ptr.setsize(incomingImage.byteCount())
         arr = np.array(ptr).reshape(height, width, 4)  #  Copies the data
         return arr
-
-
+        
+    def detectLines(self, im):
+        rows,cols = im.shape[:2]
+        imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+        ret,thresh = cv2.threshold(imgray,125,255,0)
+        thresh = (255-thresh)
+        im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        contnumber=0
+        image = np.zeros((128, 128, 3), np.uint8)
+        image[:] = (255, 255, 255)
+        for contour in contours:
+            if(len(contour) >= 5):
+                cv2.drawContours(image, contours, contnumber, (0,0,255), 1) #draw only contour contnumber
+            contnumber+=1
+                
+        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray,50,100,apertureSize = 3)
+        minLineLength = 10
+        maxLineGap = 2
+        lines = cv2.HoughLinesP(edges,0.01,np.pi/180,1,minLineLength,maxLineGap)
+        return lines        
 
 class Window(QtGui.QMainWindow):
     def __init__(self):
@@ -112,19 +133,84 @@ if __name__ == '__main__':
 
 
 
-### WIP
+### PLAYGROUND
 
-
+###HOUGH
 
 #imgpath = 'C:/Users/sayegh/test.jpg'
 #img = cv2.imread(imgpath)
 #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#edges = cv2.Canny(gray,50,150,apertureSize = 3)
-#minLineLength = 10
-#maxLineGap = 10
-#lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
-#print lines
-#for x1,y1,x2,y2 in lines[0]:
-#    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+##edges = cv2.Canny(gray,50,100,apertureSize = 3)
+#minLineLength = 5
+#maxLineGap = 20
+#lines = cv2.HoughLinesP(gray,0.1,np.pi/180,20,minLineLength,maxLineGap)
+##for line in lines:
+##    for x1,y1,x2,y2 in line:
+##        cv2.line(img,(x1,y1),(x2,y2),(0,255,0),1)
+##        print x1,  y1, x2, y2
+##cv2.imwrite('res.jpg',img)
+
+
+
+#####CONTOURS
+#im = cv2.imread('C:/Users/sayegh/testImage.jpg')
+#rows,cols = im.shape[:2]
+#imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+#ret,thresh = cv2.threshold(imgray,125,255,0)
+#thresh = (255-thresh)
+#thresh2=thresh.copy()
+#im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 #
-#cv2.imwrite('hough.jpg',img)
+##cv2.imshow('image1',im)
+##cv2.imshow('image3',thresh2)
+##cv2.drawContours(im, contours, -1, (0,255,0), 1) #draw all contours
+#contnumber=0
+#image = np.zeros((128, 128, 3), np.uint8)
+#image[:] = (255, 255, 255)
+#
+#
+#for contour in contours:
+#    if(len(contour) >= 5):
+#        print "Contour:", contour
+#        cv2.drawContours(image, contours, contnumber, (0,0,255), 1) #draw only contour contnumber
+#
+#    contnumber+=1
+#    
+#cv2.imwrite('res.jpg',image)
+#
+#
+#imgpath = 'res.jpg'
+#img = cv2.imread(imgpath)
+#gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#edges = cv2.Canny(gray,50,100,apertureSize = 3)
+#cv2.imwrite('gray.jpg',edges)
+#
+#minLineLength = 10
+#maxLineGap = 2
+#lines = cv2.HoughLinesP(edges,0.01,np.pi/180,15,minLineLength,maxLineGap)
+#print len(lines)
+#for line in lines:
+#    for x1,y1,x2,y2 in line:
+#        cv2.line(img,(x1,y1),(x2,y2),(0,255,0),1)
+#        print x1, y1, x2, y2
+#cv2.imwrite('res3.jpg',img)
+#
+##
+##minLineLength = 5
+##maxLineGap = 20
+##gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+##
+##lines = cv2.HoughLinesP(gray,0.1,np.pi/180,20,minLineLength,maxLineGap)
+##for line in lines:
+##    for x1,y1,x2,y2 in line:
+##        cv2.line(image,(x1,y1),(x2,y2),(0,255,0),1)
+##        print x1,  y1, x2, y2
+##cv2.imwrite('res2.jpg',image)
+##index = 0
+##for contour in contours:
+##    [vx,vy,x,y] = cv2.fitLine(contours[index], cv2.DIST_L2,0,0.01,0.01)
+##    lefty = int((-x*vy/vx) + y)
+##    index+=1
+##    righty = int(((cols-x)*vy/vx)+y)
+##    cv2.line(im,(cols-1,righty),(0,letfty),(0,255,255),2)
+#
